5063e4e03a14cbc4602f7c9ab860a4937b7492db
Martin Klefas-Stennett <cleverbum@hotmail.com>
2023-03-03 14:09:52 +0000

    Milestone 4 Task 2 - modularized

--- manual_rps.py
+++ manual_rps.py
@@ -6,10 +6,10 @@ def get_computer_choice():
 
 def get_user_choice():
     while True:
-        guess = input("Please choose your play: ")
+        guess = input("Please choose your play: ").lower()
 
         try:
-            if guess in ["Rock","Paper","Scissors"]:  
+            if guess in ["rock","paper","scissors"]:  
                 break
             else:
                 print("Invalid Choice. Please choose from:", choices)
@@ -18,4 +18,38 @@ def get_user_choice():
 
     return guess
 
-print(get_computer_choice())
+def get_winner(computer_choice, user_choice):
+    if computer_choice == "Rock":
+        if user_choice == "rock":
+            return "draw"
+        elif user_choice == "paper":
+            return "win"
+        else:
+            return "lose"
+        
+    elif computer_choice == "Paper":
+        if user_choice == "rock":
+            return "lose"
+        elif user_choice == "paper":
+            return "draw"
+        else:
+            return "win"
+    else:
+        if user_choice == "rock":
+            return "win"
+        elif user_choice == "paper":
+            return "lose"
+        else:
+            return "draw"
+        
+def print_result(result):
+    if result == "win":
+        print("You lost")
+    elif result == "lose":
+        print("You won!")
+    else:
+        print("It is a tie!")
+
+game_result = get_winner(get_computer_choice(),get_user_choice())
+
+print_result(game_result)
