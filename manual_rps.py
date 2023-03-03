fae6c1025860f992623f90ef6cd56a68884d3bbe
Martin Klefas-Stennett <cleverbum@hotmail.com>
2023-03-03 14:47:47 +0000

    Should complete milestone 4

--- manual_rps.py
+++ manual_rps.py
@@ -43,5 +43,8 @@ def get_winner(computer_choice, user_choice):
             print("It is a tie!")
         
 
-get_winner(get_computer_choice(),get_user_choice())
+def play():
+    get_winner(get_computer_choice(),get_user_choice())
+
+play()
 
