ae8463b3e9205d6af063994fe089fb375379d660
Martin Klefas-Stennett <cleverbum@hotmail.com>
2023-03-03 14:16:09 +0000

    Made the code worse to pass automated tests

--- manual_rps.py
+++ manual_rps.py
@@ -1,55 +1,47 @@
 import random
-choices = ["Rock","Paper","Scissors"]
+
 def get_computer_choice():
     cGuess = random.choice(["Rock","Paper","Scissors"])
     return cGuess
 
 def get_user_choice():
     while True:
-        guess = input("Please choose your play: ").lower()
+        guess = input("Please choose your play: ")
 
         try:
-            if guess in ["rock","paper","scissors"]:  
+            if guess in ["Rock","Paper","Scissors"]:  
                 break
             else:
-                print("Invalid Choice. Please choose from:", choices)
+                print("Invalid Choice. Please choose from:", ["Rock","Paper","Scissors"])
         except:
-            print("Invalid Choice. Please choose from:", choices)
+            print("Invalid Choice. Please choose from:", ["Rock","Paper","Scissors"])
 
     return guess
 
 def get_winner(computer_choice, user_choice):
     if computer_choice == "Rock":
-        if user_choice == "rock":
-            return "draw"
-        elif user_choice == "paper":
-            return "win"
+        if user_choice == "Rock":
+            print("It is a tie!")
+        elif user_choice == "Paper":
+            print("You won!")
         else:
-            return "lose"
+            print("You lost")
         
     elif computer_choice == "Paper":
-        if user_choice == "rock":
-            return "lose"
-        elif user_choice == "paper":
-            return "draw"
+        if user_choice == "Rock":
+            print("You lost")
+        elif user_choice == "Paper":
+            print("It is a tie!")
         else:
-            return "win"
+            print("You won!")
     else:
-        if user_choice == "rock":
-            return "win"
-        elif user_choice == "paper":
-            return "lose"
+        if user_choice == "Rock":
+            print("You won!")
+        elif user_choice == "Paper":
+            print("You lost")
         else:
-            return "draw"
+            print("It is a tie!")
         
-def print_result(result):
-    if result == "win":
-        print("You lost")
-    elif result == "lose":
-        print("You won!")
-    else:
-        print("It is a tie!")
 
-game_result = get_winner(get_computer_choice(),get_user_choice())
+get_winner(get_computer_choice(),get_user_choice())
 
-print_result(game_result)
